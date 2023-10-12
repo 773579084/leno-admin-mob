e413881cef5f2e58d7d67cc4878ac94256931605
unknown <773579084@qq.com>
2023-10-12 17:18:03 +0800

    ci:login

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Button, Text } from "@tarojs/components";
 import { useEffect, useState } from "react";
+import "./index.scss";
 
 function Index() {
   return <View>首页</View>;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,59 @@
 import { View, Text, Image } from "@tarojs/components";
 import { useEffect, useState } from "react";
+import { AtForm, AtInput, AtButton } from "taro-ui";
 import logPng from "../../assets/images/logo.png";
+import "./index.scss";
 
 function Login() {
   return (
-    <View className="at-row">
-      <View className="at-col">
-        <Image
-          style="width: 60px;height: 50px;background: #fff;"
-          src={logPng}
-        />
+    <View>
+      <View className="at-row at-row__justify--center logo-title">
+        <View className="flex-align">
+          <Image
+            style="width: 40px;height: 35px;background: #fff;margin-right:5px;"
+            src={logPng}
+          />
+        </View>
+        <View className="flex-align">
+          <Text>leno-admin移动端登录</Text>
+        </View>
       </View>
-      <View className="at-col">
-        <Text>leno-admin移动端登录</Text>
+      <View className="at-row at-row__justify--center">
+        <AtForm onSubmit={() => {}}>
+          <AtInput
+            name="value"
+            title="文本"
+            type="text"
+            placeholder="单行文本"
+            value={""}
+            onChange={() => {}}
+          />
+          <AtInput
+            name="value"
+            title="文本"
+            type="text"
+            placeholder="单行文本"
+            value={""}
+            onChange={() => {}}
+          />
+          <View className="at-row at-row__justify--center">
+            <AtInput
+              name="value"
+              title="文本"
+              type="text"
+              placeholder="单行文本"
+              value={""}
+              onChange={() => {}}
+            />
+            <Image style="width: 100px;height: 40px;" src={logPng} />
+          </View>
+          <AtButton formType="submit" circle type="primary">
+            提交
+          </AtButton>
+        </AtForm>
+      </View>
+      <View className="at-row at-row__justify--center login-deal">
+        登录即代表同意《用户协议》《隐私协议》
       </View>
     </View>
   );
