e52b564201acf1b90e94a77525bb2641957280fa
unknown <773579084@qq.com>
2023-12-20 11:04:58 +0800

    ci:我的页面静态完成

--- src/pages/main/my/index.tsx
+++ src/pages/main/my/index.tsx
@@ -1,8 +1,12 @@
-import { View } from "@tarojs/components";
+import { View, Image } from "@tarojs/components";
 import { useEffect, useState } from "react";
-import { AtAvatar, AtIcon } from "taro-ui";
+import { AtAvatar, AtIcon, AtList, AtListItem } from "taro-ui";
 import NavBar from "~/components/NavBar";
 import "./index.scss";
+import github from "~/assets/images/github.png";
+import guanzhu from "~/assets/images/关注.png";
+import wendang from "~/assets/images/文档.png";
+import people from "~/assets/images/人工成本.png";
 
 function My() {
   return (
@@ -19,27 +23,49 @@ function My() {
         <View className="bottom">
           <View className="bottom-one">
             <View className="card">
-              <AtIcon
-                value="rengongchengben"
-                size="30"
-                prefixClass="iconfont"
-              />
+              <Image className="icon" src={people} />
               交流群
             </View>
             <View className="card">
-              <AtIcon value="github" size="30" prefixClass="iconfont" />
+              <Image className="icon" src={github} />
               Github
             </View>
             <View className="card">
-              <AtIcon value="wendang" size="30" prefixClass="iconfont" />
+              <Image className="icon" src={wendang} />
               技术文档
             </View>
             <View className="card">
-              <AtIcon value="guanzhu" size="30" prefixClass="iconfont" />
+              <Image className="icon" src={guanzhu} />
               关注作者
             </View>
           </View>
-          <View className="bottom-two">2-2</View>
+          <View className="bottom-two">
+            <AtList>
+              <AtListItem
+                title="编辑资料"
+                arrow="right"
+                iconInfo={{ size: 25, color: "#78A4FA", value: "user" }}
+              />
+
+              <AtListItem
+                title="常见问题"
+                arrow="right"
+                iconInfo={{ size: 25, color: "#78A4FA", value: "help" }}
+              />
+
+              <AtListItem
+                title="关于我们"
+                arrow="right"
+                iconInfo={{ size: 25, color: "#78A4FA", value: "heart" }}
+              />
+
+              <AtListItem
+                title="应用设置"
+                arrow="right"
+                iconInfo={{ size: 25, color: "#78A4FA", value: "settings" }}
+              />
+            </AtList>
+          </View>
         </View>
       </View>
     </View>
